3c6e15b48f3e1919fcb05ce6c4d380869d7d7fc7
iwamatsu-kana <2828cdmpsakura@au.com>
2024-03-08 20:51:50 +0900

    packageを作成しました。

---
+++ template/src/com/diworksdev/template/action/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Owner
+ *
+ */
+package com.diworksdev.template.action;
---
+++ template/src/com/diworksdev/template/dao/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Owner
+ *
+ */
+package com.diworksdev.template.dao;
